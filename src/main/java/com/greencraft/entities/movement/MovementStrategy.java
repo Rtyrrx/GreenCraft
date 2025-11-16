e4a989db5887839f50adb190c464a97e1c61771e
Sanzhar Baiserikov <2006michigun2006@gmail.com>
2025-11-16 23:15:34 +0500

    Update MovementStrategy.java

--- src/main/java/com/greencraft/entities/movement/MovementStrategy.java
+++ src/main/java/com/greencraft/entities/movement/MovementStrategy.java
@@ -1,8 +1,6 @@
 package com.greencraft.entities.movement;
-
 import com.greencraft.entities.Player;
 import javafx.scene.input.KeyCode;
-
 public interface MovementStrategy {
     void move(Player player);
     void moveKey(Player player, KeyCode key);
