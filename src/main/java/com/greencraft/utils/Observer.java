4403af2c626329211fe31695d02f1c93b5b1636d
Sanzhar Baiserikov <2006michigun2006@gmail.com>
2025-11-16 23:14:18 +0500

    Update Observer.java

--- src/main/java/com/greencraft/utils/Observer.java
+++ src/main/java/com/greencraft/utils/Observer.java
@@ -3,20 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 public class Observer {
-    private static Observer instance = new Observer();
     private final List<Consumer<String>> listeners = new ArrayList<>();
-    private Observer() {
-        
+    public void subscribe(Consumer<String> listener) {
+        listeners.add(listener);
     }
-    public static Observer getInstance() { 
-        return instance; 
-    }
-    public void subscribe(Consumer<String> listener) { 
-        listeners.add(listener); 
-    }
-    public void publish(String event) { 
+    public void publish(String event) {
         for(Consumer<String> l:listeners) {
-            l.accept(event); 
+            l.accept(event);
         }
     }
 }
