2e725e1fd80646928df01fe482fc047bf030451f
Sanzhar Baiserikov <2006michigun2006@gmail.com>
2025-11-16 23:24:56 +0500

    Update WaterFactory.java

--- src/main/java/com/greencraft/blocks/blockFactory/WaterFactory.java
+++ src/main/java/com/greencraft/blocks/blockFactory/WaterFactory.java
@@ -1,5 +1,4 @@
 package com.greencraft.blocks.blockFactory;
-
 import com.greencraft.blocks.Block;
 import com.greencraft.blocks.BlockType;
 import com.greencraft.blocks.sound.WaterSoundStrategy;
@@ -9,29 +8,24 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
-
 public class WaterFactory implements BlockFactory {
     public Block createBlock(BlockType blockType) {
         Box box = new Box(1, 1, 1);
         box.setTranslateY(2);
-
         // Use Singleton ResourceManager to load texture
         PhongMaterial material = new PhongMaterial();
         Image texture = ResourceManager.getInstance().getTexture("water.png");
-
-        if (texture != null) {
+        if(texture != null) {
             material.setDiffuseMap(texture);
-        } else {
+        } 
+        else {
             // Fallback to color if texture not found
             material.setDiffuseColor(Color.DEEPSKYBLUE);
         }
-
         box.setMaterial(material);
         Block b = new SimpleBlock(box, BlockType.WATER);
-
         // Assign sound strategy (Strategy Pattern)
         b.setSoundStrategy(new WaterSoundStrategy());
-
         EventLogger.log("Created WATER block");
         return b;
     }
